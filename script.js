8041453fbee5dda37d71803babb78fb56cc54e7e
SrdTibo <boncybee@gmail.com>
2022-03-11 18:11:17 +0100

    a

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
-var callBackGetSuccess = function(data){
-    console.log("donnée api", data)
-    alert("test " + data.parameters.format)
+var getHttpRequest = function () {
+    var httpRequest = false;
+
+    if (window.XMLHttpRequest) { // Mozilla, Safari,...
+        httpRequest = new XMLHttpRequest();
+        if (httpRequest.overrideMimeType) {
+            httpRequest.overrideMimeType('text/xml');
+        }
+    }
+    else if (window.ActiveXObject) { // IE
+        try {
+            httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+        }
+        catch (e) {
+            try {
+                httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            catch (e) { }
+        }
+    }
+
+    if (!httpRequest) {
+        alert('Abandon :( Impossible de créer une instance XMLHTTP');
+        return false;
+    }
+
+    return httpRequest
 }
-var url="https://data.iledefrance.fr/api/records/1.0/search/?dataset=prix-des-carburants-j-7&q=&facet=cp&facet=pop&facet=city&facet=automate_24_24&facet=fuel&facet=shortage&facet=update&facet=services&facet=brand&refine.city=ARGENTEUIL"
-$.get(url, callBackGetSuccess).done(function(){
 
-})
-.fail(function(){
-    alert("error");
-})
-.always(function(){
+var xhr = getHttpRequest()
+xhr.open('GET', 'https://cors-anywhere.herokuapp.com/https://donnees.roulez-eco.fr/opendata/instantane', true)
+// On envoit un header pour indiquer au serveur que la page est appellée en Ajax
+xhr.setRequestHeader('X-Requested-With', 'xmlhttprequest')
+// On lance la requête
+xhr.send()
 
-});
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+            xhr.responseText // contient le résultat de la page
+        } else {
+            // Le serveur a renvoyé un status d'erreur
+        }
+    }
+}
